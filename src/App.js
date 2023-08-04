e9bdd18239ad65db89c0e28d12a247cd637ad605
Aashish Gupta <aashishg863@gmail.com>
2023-08-04 20:11:04 +0530

    App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/Navbar';
+import Newscontainer from './components/newscontainer';
+function App() {
+  return (
+    <div >
+     <Navbar/>
+     <Newscontainer/>
+    </div>
+  );
+}
+
+export default App;
